e105f9f6c7ca7cb7e1be19b42c754c875213123f
Vsevolod Kryazhev <kryazhev_seva@mail.ru>
2024-11-18 08:17:29 +0500

    Add config

---
+++ shelter_service/config.py
@@ -0,0 +1,5 @@
+from dataclasses import dataclass
+from environs import Env
+
+@
+class DBConfig
---
+++ shelter_service/repositories/postgres/settings.py
@@ -0,0 +1,4 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from config import
+engine = create_async_engine()
